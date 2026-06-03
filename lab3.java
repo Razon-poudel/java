a1cd8a4c5d9ad0d2eb96aea4b68f1799ec1323b1
Razon-poudel <be2024ce281@gces.edu.np>
2026-06-03 16:47:26 +0545

    Added Lab3.java

---
+++ lab3.java
@@ -0,0 +1,49 @@
+
+
+interface LibraryHelper {
+    void checkBooks();
+}
+
+abstract class Student {
+    String name;
+    int marks;
+
+    Student(String n, int m) {
+        name = n;
+        marks = m;
+    }
+
+    void attendClass() {
+        System.out.println(name + " attending class");
+    }
+
+    int gradeCalculator() {
+        return marks / 25;
+    }
+
+    abstract void display();
+}
+
+class lab3 extends Student implements LibraryHelper {
+
+    lab3(String n, int m) {
+        super(n, m);
+    }
+
+    public void display() {
+        System.out.println("Name: " + name);
+        System.out.println("Marks: " + marks);
+        System.out.println("Grade: " + gradeCalculator());
+    }
+
+    public void checkBooks() {
+        System.out.println(name + " is checking books");
+    }
+
+    public static void main(String[] args) {
+        lab3 obj = new lab3("Naman", 75);
+        obj.display();
+        obj.attendClass();
+        obj.checkBooks();
+    }
+}     
